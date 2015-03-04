299dcb7242613c06376e28f08b8f4f7b5642666f
Yan Xu <xuyantc@msn.com>
2015-03-03 23:01:15 -0500

    add line between maker

--- app/src/main/java/com/example/fallflame/itineraryrecorder/ItineraryMapsActivity.java
+++ app/src/main/java/com/example/fallflame/itineraryrecorder/ItineraryMapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.fallflame.itineraryrecorder;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.location.Location;
@@ -22,7 +24,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -31,22 +36,28 @@ import java.util.Date;
 
 public class ItineraryMapsActivity extends FragmentActivity {
 
+    // some constant use by android system
     private static final int MEDIA_TYPE_IMAGE = 1;
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
-    // Default values
-    final private double DEFAULT_lNG = -73.597929;
-    final private double DEFAULT_LAT = 45.508536;
-    final private int DEFAULT_ZOOM_LEVEL = 12;
-    final private int DEFAULT_MARK_INTERVAL = 60;
+    // Default values, for test
+    final static private double DEFAULT_lNG = -73.597929;
+    final static private double DEFAULT_LAT = 45.508536;
+    final static private int DEFAULT_ZOOM_LEVEL = 12;
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
-    private String mode = "Cellular"; //"GPS" or "Cellular"
+    private String mode = "GPS"; //"GPS" or "Cellular", emulator can only use GPS, for test
+    private int markInterval = 600; // a default interval, for test
+
+    // used for store the
     private Location currentLocation;
+    private double currentLocationTimestamp;
+    final static private int validityOfCurrentLocation = 30000; //30s
+
     private ArrayList<ItineraryMark> itineraryMarks = new ArrayList<>();
     private ArrayList<BaseStationMark> baseStationMarks = new ArrayList<>();
-    private int markInterval = DEFAULT_MARK_INTERVAL;
+
     private CountDownTimer countDownTimer;
 
     @Override
@@ -55,7 +66,7 @@ public class ItineraryMapsActivity extends FragmentActivity {
         setContentView(R.layout.activity_itinerary_maps);
         setUpMapIfNeeded();
         resetCountDownTimer();
-        //registerLocationListener();
+        registerLocationListener();
     }
 
     @Override
@@ -99,44 +110,94 @@ public class ItineraryMapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(DEFAULT_LAT, DEFAULT_lNG), DEFAULT_ZOOM_LEVEL));
+        mMap.setMyLocationEnabled(true);
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
 
-        // add a marker
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                // Getting view from the layout file info_window_layout
+                View v = getLayoutInflater().inflate(R.layout.info_window_layout, null);
+
+                // Getting the position from the marker
+                LatLng latLng = marker.getPosition();
+
+                // Getting reference to the TextView to set latitude
+                TextView tvLat = (TextView) v.findViewById(R.id.tv_lat);
+
+                // Getting reference to the TextView to set longitude
+                TextView tvLng = (TextView) v.findViewById(R.id.tv_lng);
+
+                // Setting the latitude
+                tvLat.setText("Latitude:" + latLng.latitude);
+
+                // Setting the longitude
+                tvLng.setText("Longitude:"+ latLng.longitude);
+
+                // Returning the view containing InfoWindow contents
+                return v;
+                            }
+        });
     }
+
+
 
     public void makeMark(){
 
-        // get current battery percentage
-        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus =  getApplicationContext().registerReceiver(null, ifilter);
-        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-        float batteryPct = level / (float)scale;
+        if (currentLocation == null
+                || System.currentTimeMillis() - currentLocationTimestamp > validityOfCurrentLocation) {
 
-        ItineraryMark mark = new ItineraryMark();
+            CharSequence text = "Cannot get the current position.";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+            toast.show();
 
-        mark.setBatteryLevel(batteryPct);
-        mark.setMode(mode);
-
-        if (itineraryMarks.size() > 0)
-            mark.setPreviousMark(itineraryMarks.get(itineraryMarks.size() - 1));
-/*
-        if (currentLocation != null){
-            mark.setPosition(currentLocation.getLatitude(), currentLocation.getLongitude(), currentLocation.getAltitude());
         } else {
-            // TODO
-        }
-*/
-        itineraryMarks.add(mark);
 
-        takePhoto();
+            ItineraryMark mark = new ItineraryMark();
+            mark.setPosition(currentLocation.getLatitude(),
+                             currentLocation.getLongitude(),
+                             currentLocation.getAltitude());
+            mark.setBatteryLevel(getCurrentBatteryPercentage());
+            mark.setMode(mode);
+            if(!itineraryMarks.isEmpty())
+                mark.setPreviousMark(itineraryMarks.get(itineraryMarks.size() - 1));
+            itineraryMarks.add(mark);
+            takePhoto(); // take photo will always add the Uri to the last element in itineraryMarks array.
+            addMarkerToMap(mark); // add a marker
+
+        }
+
         resetCountDownTimer();
     }
 
     public void makeMark(View view) {
         makeMark();
+    }
+
+    private void addMarkerToMap(ItineraryMark mark){
+        if (mMap != null){
+            mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(mark.getPosition()[0], mark.getPosition()[1]))
+                                .title("I-" + itineraryMarks.size()));
+            if(itineraryMarks.size() >=2) {
+                ItineraryMark p1 = itineraryMarks.get(itineraryMarks.size()-2);
+                ItineraryMark p2 = itineraryMarks.get(itineraryMarks.size()-1);
+                double lat1 = p1.getPosition()[0];
+                double lng1 = p1.getPosition()[1];
+                double lat2 = p2.getPosition()[0];
+                double lng2 = p2.getPosition()[1];
+                PolylineOptions lineOptions = new PolylineOptions()
+                        .add(new LatLng(lat1, lng1))
+                        .add(new LatLng(lat2, lng2));
+                Polyline polyline = mMap.addPolyline(lineOptions);
+            }
+        }
     }
 
     private void registerLocationListener(){
@@ -146,7 +207,7 @@ public class ItineraryMapsActivity extends FragmentActivity {
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
-                makeUseOfNewLocation(location);
+                updateLocation(location);
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {}
@@ -166,12 +227,37 @@ public class ItineraryMapsActivity extends FragmentActivity {
         locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
     }
 
-    private void makeUseOfNewLocation(Location location){
-        this.currentLocation = location;
+    private float getCurrentBatteryPercentage(){
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus =  getApplicationContext().registerReceiver(null, ifilter);
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        return level / (float)scale;
+    }
+
+    private void updateLocation(Location location){
+        currentLocation = location;
+        currentLocationTimestamp = System.currentTimeMillis();
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), DEFAULT_ZOOM_LEVEL));
     }
 
     private void takePhoto(){
+        new AlertDialog.Builder(this).setTitle("Take a photo?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        changeToImageCaptureActivity();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {}
+                })
+                .show();
+    }
+
+    private void changeToImageCaptureActivity(){
+
         // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -221,6 +307,9 @@ public class ItineraryMapsActivity extends FragmentActivity {
     }
 
     private void resetCountDownTimer(){
+
+        if (countDownTimer != null)
+            countDownTimer.cancel();
 
         countDownTimer = new CountDownTimer(markInterval * 1000, 1000) {
             @Override
